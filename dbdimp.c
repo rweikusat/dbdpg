f62d27d9431a805090d181b94728bc7130e0921d
Rainer Weikusat <rweikusat@talktalk.net>
2025-05-11 18:57:07 +0100

    removed unused is_high_bit_set prototype

--- dbdimp.c
+++ dbdimp.c
@@ -91,7 +91,6 @@ static int pg_db_rollback_commit (pTHX_ SV *dbh, imp_dbh_t *imp_dbh, int action)
 static SV *pg_st_placeholder_key (imp_sth_t *imp_sth, ph_t *currph, int i);
 static void pg_st_split_statement (pTHX_ imp_sth_t *imp_sth, char *statement);
 static int do_pg_st_prepare_statement (pTHX_ SV *sth, imp_sth_t *imp_sth, int async);
-static int is_high_bit_set(pTHX_ const unsigned char *val, STRLEN size);
 static int pg_st_deallocate_statement(pTHX_ SV *sth, imp_sth_t *imp_sth);
 static PGTransactionStatusType pg_db_txn_status (pTHX_ imp_dbh_t *imp_dbh);
 static int pg_db_start_txn (pTHX_ SV *dbh, imp_dbh_t *imp_dbh);
