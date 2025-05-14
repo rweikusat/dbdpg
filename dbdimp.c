c696383f52d53c636cfefea71d3eb09fb7b94bf9
Rainer Weikusat <rweikusat@talktalk.net>
2025-05-14 11:49:37 +0100

    imp_dbh->async_sth may be NULL in pg_db_ready when the query was sent via quickexec

--- dbdimp.c
+++ dbdimp.c
@@ -5497,38 +5497,35 @@ int pg_db_ready(SV *h, imp_dbh_t *imp_dbh)
     }
 
     imp_sth = imp_dbh->async_sth;
-    if (imp_sth->async_status == STH_ASYNC) {
-        ret = 1;
-        goto out;
+    if (imp_sth && STH_ASYNC_PREPARE == imp_sth->async_status) {
+        status = PGRES_COMMAND_OK;
+        TRACE_PQGETRESULT;
+        while ((result = PQgetResult(imp_dbh->conn))) {
+            ret = _sqlstate(aTHX_ imp_dbh, result);
+            if (ret != PGRES_COMMAND_OK) status = ret;
+
+            PQclear(result);
+        }
+        if (status != PGRES_COMMAND_OK) {
+            Safefree(imp_sth->prepare_name);
+            imp_sth->prepare_name = NULL;
+
+            pg_func = "PQsendPrepare";
+            goto pg_error;
+        }
+
+        imp_sth->prepared_by_us = DBDPG_TRUE;
+        ++imp_dbh->prepare_number;
+
+        ret = pq_send_prepared_query(aTHX_ imp_dbh, imp_sth);
+        if (!ret) {
+            pg_func = "PQsendQueryPrepared";
+            goto pg_error;
+        }
+
+        ret = 0;
+        imp_sth->async_status = STH_ASYNC;
     }
-
-    status = PGRES_COMMAND_OK;
-    TRACE_PQGETRESULT;
-    while ((result = PQgetResult(imp_dbh->conn))) {
-        ret = _sqlstate(aTHX_ imp_dbh, result);
-        if (ret != PGRES_COMMAND_OK) status = ret;
-
-        PQclear(result);
-    }
-    if (status != PGRES_COMMAND_OK) {
-        Safefree(imp_sth->prepare_name);
-        imp_sth->prepare_name = NULL;
-
-        pg_func = "PQsendPrepare";
-        goto pg_error;
-    }
-
-    imp_sth->prepared_by_us = DBDPG_TRUE;
-    ++imp_dbh->prepare_number;
-
-    ret = pq_send_prepared_query(aTHX_ imp_dbh, imp_sth);
-    if (!ret) {
-        pg_func = "PQsendQueryPrepared";
-        goto pg_error;
-    }
-
-    ret = 0;
-    imp_sth->async_status = STH_ASYNC;
 
 out:
     if (TEND_slow) TRC(DBILOGFP, "%sEnd pg_db_ready\n", THEADER_slow);
