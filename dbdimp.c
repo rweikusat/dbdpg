b6b99d04507ae44009b81fe07839ecb64879d21d
Rainer Weikusat <rweikusat@talktalk.net>
2025-06-17 12:07:07 +0100

    need to check for STH_ASYNC_PREPARE in dbd_st_prepare and not STH_ASYNC

--- dbdimp.c
+++ dbdimp.c
@@ -1899,7 +1899,7 @@ int dbd_st_prepare_sv (SV * sth, imp_sth_t * imp_sth, SV * statement_sv, SV * at
             croak ("%s", PQerrorMessage(imp_dbh->conn));
         }
 
-        if (STH_ASYNC == imp_sth->async_status)
+        if (STH_ASYNC_PREPARE == imp_sth->async_status)
             imp_dbh->async_status = DBH_ASYNC;
     }
 
