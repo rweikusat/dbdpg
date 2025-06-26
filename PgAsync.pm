33d5ad082214546f85021b705ece16f6b26f8c4e
Rainer Weikusat <rweikusat@talktalk.net>
2025-06-26 19:17:46 +0100

    bump version to 0.4.2

--- PgAsync.pm
+++ PgAsync.pm
@@ -16,7 +16,7 @@ use 5.008001;
 {
     package DBD::PgAsync;
 
-    use version; our $VERSION = qv('0.4.1');
+    use version; our $VERSION = qv('0.4.2');
 
     use DBI 1.614 ();
     use Exporter ();
