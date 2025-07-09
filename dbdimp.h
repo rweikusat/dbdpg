d6daa2adfdbf3640286155f2eb7ed70b691fa28c
Rainer Weikusat <rweikusat@talktalk.net>
2025-07-09 19:10:58 +0100

    turned after_success into a structure as the callback needs a second argument

--- dbdimp.h
+++ dbdimp.h
@@ -62,7 +62,11 @@ struct imp_dbh_st {
     imp_sth_t *do_tmp_sth;      /* temporary sth to refer inside a do() call */
     async_action_t *aa_first, **aa_pp; /* list of asynchronous actions which need to be done before pg_ready can return true */
     int       use_async;               /* use async operations for everything */
-    void      (*after_success)(imp_dbh_t *); /* run after success of an async query */
+
+    struct {
+        void (*cb)(imp_dbh_t *, void *);
+        void *arg;
+    } after_success;
 };
 
 /* Each statement is broken up into segments */
