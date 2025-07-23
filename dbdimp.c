400b9ff66e5e275d553c209550fdf365bbc96365
Rainer Weikusat <rweikusat@talktalk.net>
2025-07-23 18:51:36 +0100

    set an async result handler when preparing a statement

--- dbdimp.c
+++ dbdimp.c
@@ -2807,9 +2807,11 @@ static int pg_st_prepare_statement (pTHX_ SV * sth, imp_sth_t * imp_sth)
         TRACE_PQSENDPREPARE;
         status = PQsendPrepare(imp_dbh->conn, imp_sth->prepare_name, statement, params,
                                imp_sth->PQoids);
-        if (status) 
+        if (status) {
             imp_sth->async_status = STH_ASYNC_PREPARE;
-        else {
+            imp_dbh->async_result.handler = handle_query_result;
+            imp_dbh->async_result.arg = NULL;
+        } else {
             status = PGRES_FATAL_ERROR;
             _fatal_sqlstate(aTHX_ imp_dbh);
         }
