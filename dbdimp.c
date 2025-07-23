a52ee2ac43c5c0ef4bb1dbf4c10fc112f95efc86
Rainer Weikusat <rweikusat@talktalk.net>
2025-07-23 13:07:06 +0100

    initialized rows to zero in handle_query_result

--- dbdimp.c
+++ dbdimp.c
@@ -5757,6 +5757,7 @@ static long handle_query_result(PGresult *result, int status, SV *h, imp_dbh_t *
     char *cmdStatus;
     long rows;
 
+    rows = 0;
     switch ((int)status) {
     case PGRES_TUPLES_OK:
         TRACE_PQNTUPLES;
