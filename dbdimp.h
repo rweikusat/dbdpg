b9ccf0503fa2d13db12d6a8a442f023649748a07
Rainer Weikusat <rweikusat@talktalk.net>
2025-08-08 13:06:15 +0100

    fixed some errors in dealloc type definition

--- dbdimp.h
+++ dbdimp.h
@@ -32,12 +32,12 @@ struct async_action_st {
 
 typedef struct async_action_st async_action_t;
 
-struct decalloc_st {
+struct dealloc_st {
     struct dealloc_st *p;
     char *name;
 };
 
-typedef dealloc_st dealloc_t;
+typedef struct dealloc_st dealloc_t;
 
 struct imp_dbh_st {
     dbih_dbc_t com;            /* MUST be first element in structure */
